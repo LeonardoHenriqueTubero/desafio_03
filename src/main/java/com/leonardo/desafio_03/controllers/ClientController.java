3dc3ec57da1b1b24af0f4e371b3e475a6f2967a9
Leonardo Henrique Pereira Tubero <106193658+LeonardoHenriqueTubero@users.noreply.github.com>
2025-08-21 11:15:01 -0300

    Find all

--- src/main/java/com/leonardo/desafio_03/controllers/ClientController.java
+++ src/main/java/com/leonardo/desafio_03/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.leonardo.desafio_03.controllers;
 import com.leonardo.desafio_03.dto.ClientDTO;
 import com.leonardo.desafio_03.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,5 +26,11 @@ public class ClientController {
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         ClientDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dtos = service.findAll(pageable);
+        return ResponseEntity.ok(dtos);
     }
 }
--- src/main/java/com/leonardo/desafio_03/services/ClientService.java
+++ src/main/java/com/leonardo/desafio_03/services/ClientService.java
@@ -5,6 +5,8 @@ import com.leonardo.desafio_03.entities.Client;
 import com.leonardo.desafio_03.repositories.ClientRepository;
 import com.leonardo.desafio_03.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +24,12 @@ public class ClientService {
     public ClientDTO findById(Long id) {
         Client result = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado."));
         return new ClientDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(ClientDTO::new);
     }
 
     private void dtoToEntity(Client entity, ClientDTO dto) {
